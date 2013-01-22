0ce51597ecbb870ef88b11f56ebb48f6172ffafc
Luis Castro <luis.adrian@gmail.com>
2013-01-21 20:47:34 -0700

    Update spanish/migration_lang.php

--- spanish/migration_lang.php
+++ spanish/migration_lang.php
@@ -4,9 +4,9 @@ $lang['migration_none_found']			= "No se encontraron migraciones.";
 $lang['migration_not_found']			= "Esta migración no se ha encontrado.";
 $lang['migration_multiple_version']		= "Hay multiples migraciones con el mismo número de versión: %d.";
 $lang['migration_class_doesnt_exist']	= "La clase de migración\"%s\" no se ha encontrado.";
-$lang['migration_missing_up_method']	= "A la clase de migración \"%s\" le falta el m�todo 'up'.";
-$lang['migration_missing_down_method']	= "A la clase de migración \"%s\" le falta el m�todo 'down'.";
-$lang['migration_invalid_filename']		= "Migración \"%s\" tiene un nombre de archivo no v�lido..";
+$lang['migration_missing_up_method']	= "A la clase de migración \"%s\" le falta el m6todo 'up'.";
+$lang['migration_missing_down_method']	= "A la clase de migración \"%s\" le falta el método 'down'.";
+$lang['migration_invalid_filename']		= "Migración \"%s\" tiene un nombre de archivo no válido..";
 
 
 /* End of file migration_lang.php */
